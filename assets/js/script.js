3770977e3252d5608e078b261537738787b58a2f
eleazar.fuentes <eleazar.fuentes@gmail.com>
2023-07-04 23:18:20 -0600

    adding four questions to question var with answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,29 @@
 var timerID;
 var timer = document.getElementById("timer");
 var startBt = document.getElementById("start-btn");
-
 var nextBt = document.getElementById("next-btn");
 
 startBt.addEventListener("click", initQuiz);
+
+// init quiz
+function initQuiz() {
+    timerID = setInterval(timeC, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    QuestionIn = 0
+    questionContainerEl.classList.remove("hide");
+
+    // Timer will start as soon as start button is clicked
+    timeC();
+    NextQ();
+};
+
+// Countdown timer
+function timeC() {
+    timeL--;
+    timer.textContent = "Time: " + timeL;
+    if (timeL <= 0) {
+        saveSc();
+    }
+}
+
