ddf6e1819c1d75a58178ea3f79f5fa08f892545b
eleazar.fuentes <eleazar.fuentes@gmail.com>
2023-07-04 22:27:13 -0600

    adding style and initial js logic

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Initial variables
+var timerID;
+var timer = document.getElementById("timer");
+var startBt = document.getElementById("start-btn");
+
+var nextBt = document.getElementById("next-btn");
+
+startBt.addEventListener("click", initQuiz);
