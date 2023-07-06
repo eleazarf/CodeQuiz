44413f8acf2ab0fa1e266d62b015dfb8a50d0735
EleazarF@imac <eleazar.fuentes@gmail.com>
2023-07-05 22:43:46 -0600

    adding variables for question containers and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,78 @@
 var startBt = document.getElementById("start-btn");
 var nextBt = document.getElementById("next-btn");
 
-var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn");
-
+//variables related to countdown timer
 var timerID;
 var timer = document.getElementById("timer");
 var timeLeft = 75; // time left is the time for each question
 var timerEl = document.getElementById("timer"); //timer element selects timer section from DOM
 
+
 //Variables for question containers
 var startContainerEl = document.getElementById("start-container");
+var questionContainerEl = document.getElementById("question-container");
+
+//Question variables for quiz
+var questions = [
+    { 
+        question: "How do you write 'Hello World' in an alert box?", 
+        answers: [
+            { text: "msg('Hello World')", correct: false },
+            { text: "alert('Hello World')" , correct: true },
+            { text: "prompt('Hello World')", correct: false },
+            { text: "alertBox('Hello World')", correct: false }
+        ]
+    },
+    { 
+        question: "Which of the following function of Array object calls a function for each element in the array?", 
+        answers: [
+            { text: "concat()", correct: false },
+            { text: "filter()", correct: false },
+            { text: "forEach()", correct: true },
+            { text: "split()", correct: false }
+        ]
+    },
+    { 
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 8?", 
+        answers: [
+            { text: "if (i != 8)", correct: true },
+            { text: "if i =! 8", correct: false },
+            { text: "if (i <> 8)", correct: false },
+            { text: "if (i !=== 8)", correct: false }
+        ]
+    },
+    { 
+        question: "What is the correct way to write a JavaScript array?", 
+        answers: [
+            { text: "var vegetable = (0:'kale', 1:'lettuce', 2:'spinach')", correct: false },
+            { text: "var vegetable = ['kale', 'lettuce', 'spinach']", correct: true },
+            { text: "var vegetable = (kale, lettuce, spinach)", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+    { 
+        question: "How do you round the number 102.456, to the nearest integer?",
+        answers: [
+            { text: "Math.random(102.456)", correct: false },
+            { text: "Math.rnd(102.456)", correct: false },
+            { text: "round(102.456)", correct: false },
+            { text: "None of the above", correct: true }
+        ]
+    },
+];
 
 // This is the count down timer
 function countDown() {
     timeLeft--;
     timerEl.textContent = "Time: " + timeLeft;
     if (timeLeft <= 0) {
-        saveScore();
+        saveScore(); //goes to function to save score once time is up
     }
 }
 
 // This is the Start button which trigger the first question and next button to display
-startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", () => {
+startBt.addEventListener("click", startGame);
+nextBt.addEventListener("click", () => {
     currentQuestionIndex++
     setNextQuestion()
 });
@@ -41,4 +90,16 @@ function startGame() {
     // Timer will start as soon as start button is clicked
     countDown();
     setNextQuestion();
+};
+
+// saveScore function saves the score once time is up or question is answered, uses a local store variable
+function saveScore() {
+    clearInterval(timerID);
+    timerEl.textContent = "Time: " + timeLeft;
+    setTimeout(function () {
+        //localStorage.setItem("scores", JSON.stringify(scores));
+        questionContainerEl.classList.add("hide");
+        document.getElementById("score-container").classList.remove("hide");
+        document.getElementById("your-score").textContent = "Your final score is " + timeLeft;
+    }, 2000)
 };
