aacf53de8a332a290fd16c0ebf811b53e6081e53
EleazarF@imac <eleazar.fuentes@gmail.com>
2023-07-05 22:26:09 -0600

    adding html structure and initial JS logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,44 @@
 // Initial variables
-var timerID;
-var timer = document.getElementById("timer");
+//startbutton and next button variables
 var startBt = document.getElementById("start-btn");
 var nextBt = document.getElementById("next-btn");
 
-startBt.addEventListener("click", initQuiz);
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
 
-// init quiz
-function initQuiz() {
-    timerID = setInterval(timeC, 1000);
-    startContainerEl.classList.add("hide");
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    QuestionIn = 0
-    questionContainerEl.classList.remove("hide");
+var timerID;
+var timer = document.getElementById("timer");
+var timeLeft = 75; // time left is the time for each question
+var timerEl = document.getElementById("timer"); //timer element selects timer section from DOM
 
-    // Timer will start as soon as start button is clicked
-    timeC();
-    NextQ();
-};
+//Variables for question containers
+var startContainerEl = document.getElementById("start-container");
 
-// Countdown timer
-function timeC() {
-    timeL--;
-    timer.textContent = "Time: " + timeL;
-    if (timeL <= 0) {
-        saveSc();
+// This is the count down timer
+function countDown() {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+        saveScore();
     }
 }
 
+// This is the Start button which trigger the first question and next button to display
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
+
+// this is the Start Quiz function
+function startGame() {
+    timerID = setInterval(countDown, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide");
+
+    // Timer will start as soon as start button is clicked
+    countDown();
+    setNextQuestion();
+};
